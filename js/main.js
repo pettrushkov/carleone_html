8975d093de57004c89a3b3da23e3daa965c6720d
Denys Petrushkov <denis.p@cheitgroup.com>
2020-05-28 21:09:28 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function($) {
     
-   
+  $('select').niceSelect();
+
+  $('.banner__slider').slick({
+    arrows: true,
+    dots: true,
+    prevArrow: $('.banner__buttons-prev'),
+    nextArrow: $('.banner__buttons-next')
+  });
+
+  $('.cars__tabs .tab').click(function() {
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    $('.cars__tab').removeClass('active');
+    $('.cars__tab[data-tab="' + $(this).text() + '"]').addClass('active');
+  });
 
 });
 
