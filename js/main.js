b3ddd3868a913a0c13826f7167f55a1e12f041b1
Denys Petrushkov <denis.p@cheitgroup.com>
2020-06-08 21:20:44 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function($) {
-    
+jQuery(document).ready(function ($) {
+
   $('select').niceSelect();
 
   $('.banner__slider').slick({
@@ -35,20 +35,53 @@ jQuery(document).ready(function($) {
     nextArrow: $('.says__buttons-next')
   });
 
-  $('.cars__tabs .tab').click(function() {
+  $('.cars__tabs .tab').click(function () {
     $(this).siblings().removeClass('active');
     $(this).addClass('active');
     $('.cars__tab').removeClass('active');
     $('.cars__tab[data-tab="' + $(this).text() + '"]').addClass('active');
   });
 
-  $('.top10__question').click(function() {
-    if($(this).parent().siblings().hasClass('active')) {
+  $('.top10__question').click(function () {
+    if ($(this).parent().siblings().hasClass('active')) {
       $('.top10__item.active .top10__answer').not($(this)).slideUp();
       $('.top10__item.active').not($(this)).removeClass('active');
     }
     $(this).parent().toggleClass('active');
     $(this).next().slideToggle();
+  });
+
+  $('.header__search').click(function () {
+    $('.header__menu-search').addClass('active');
+  });
+
+  $('.header__menu-search-close').click(function () {
+    $(this).parent().parent().removeClass('active');
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $('#scrollToTop').addClass('show');
+    } else {
+      $('#scrollToTop').removeClass('show');
+    }
+
+    var scrollTop     = $(window).scrollTop(),
+    elementOffset = $('.header__numbers').offset().top,
+    distance      = (elementOffset - scrollTop);
+    console.log(distance);
+    
+    if(distance <= -50) {
+      $('.header__menu').addClass('sticky');
+    } else {
+      $('.header__menu').removeClass('sticky');
+      $('body').removeClass('sticky');
+    }
+  });
+
+  $('#scrollToTop').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '700');
   });
 
 });
